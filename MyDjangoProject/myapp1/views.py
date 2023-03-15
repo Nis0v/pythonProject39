cf037fad18242a714435414ee65222bda1594a71
Nis0v <nisov107@gmail.com>
2023-03-15 13:14:17 +0300

    d

--- MyDjangoProject/myapp1/views.py
+++ MyDjangoProject/myapp1/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-
+from myapp1.models import Worker
 def index_page(request):
-    return  render(request, 'index.html')
+
+    all_workers = Worker.objects.all()
+
+    return  render(request, 'index.html', {'data': all_workers})
 
 def about_page(request):
     return render(request, 'about.html')
